90b0123fba85b8a315c5396dacef7aa68403a5d1
CK <118324821+ctk-git@users.noreply.github.com>
2022-12-11 19:12:04 -0500

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,15 +6,36 @@ const Heading1 = ({text}) => <h1>{text}</h1>
 
 const PrintStats = ({text,value}) => <div>{text} {value} </div> 
 
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
+  const handleGood = () => {
+      setGood(good + 1)
+      setAll(all + 1)
+      setPositive((good + 1)/(all + 1)*100.0)
+      setAverage(((good + 1)-bad)/(all + 1))
+  }
+  const handleNeutral = () => {
+      setNeutral(neutral + 1)
+      setAll(all + 1)
+      setPositive(good/(all + 1)*100.0)
+      setAverage((good-bad)/(all + 1))
+  }
+  const handleBad = () => {
+      setBad(bad + 1)
+      setAll(all + 1)
+      setPositive(good/(all + 1)*100.0)
+      setAverage((good-(bad + 1))/(all + 1))
+  }
+
 
   return (
     <div>
@@ -26,6 +47,11 @@ const App = () => {
     <PrintStats text='good' value={good} />
     <PrintStats text='neutral' value={neutral} />
     <PrintStats text='bad' value={bad} />
+    <PrintStats text='all' value={all} />
+    <PrintStats text='average' value={average} />
+    positive {positive} %
+    
+
     </div>
   )
 }
